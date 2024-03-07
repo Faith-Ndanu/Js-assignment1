fd4c5ad11e470fd65ebf0e7f5f566c8c69a54287
Faith-Ndanu <faithndanumunyao@gmail.com>
2024-03-07 02:53:05 -0500

    added content

---
+++ strings.js
@@ -0,0 +1,67 @@
+     
+     
+    //QUESTION 1 
+let str = "ertravaganza";
+let substr = str.substring(8, );
+console.log(substr);
+
+
+
+//QUESTION 2
+const food= "The quick fox jumped over the lazy dog";
+let foodToAdd = "eat";
+let indexPosition = 4;
+ let newString = food.slice(0,indexPosition)+foodToAdd+ food.slice(indexPosition);
+console.log(newString);
+
+
+
+//QUESTION 3
+const story = "The quick fox jumped over the lazy dog";
+const variable1 = "the";
+const variable2 = "brown";
+console.count("the");
+console.count("brown");
+
+
+//QUESTION 4
+const myString = "wonderful"
+const upperCaseString = myString.toUpperCase();
+console.log(upperCaseString);
+
+var word = "amazing";
+var lowerCaseString = word.toLowerCase();
+console.log(lowerCaseString);
+
+var text = "UndERneath"
+var lowerCaseString = text.toLowerCase();
+console.log(lowerCaseString);
+
+function titleCase(str){
+    str = str.toLowerCase().split('');
+    for (let i = 0; i<str.length; i++){
+    }
+    return str.join('');
+}
+console.log(titleCase("A wonderful world"));
+
+
+//QUESTION 5
+let y = "The pupils are reading in the library";
+console.log(y.indexOf('are'));
+let z = "The child was sitting on the table before it fell"
+console.log(z.indexOf('sitting'));
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
